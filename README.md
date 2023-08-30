39af35176296f8fe5acee1105f1a0e6af2af009e
ahmedamerworks <ahmedamerworks@gmail.com>
2023-08-30 14:20:20 +0400

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Project Summary
+
+This project was an attempt at creating a daily tasks web application with working buttons and a date display that updates in real time.
+
+The goal was to create the app using React framework and style it minamally using CSS.
+
+# React + Vite
+
+This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+
+Currently, two official plugins are available:
+
+- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
+- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
---
+++ src/App.jsx
@@ -0,0 +1,95 @@
+import { useState } from "react";
+import "./main.css";
+
+export default function App() {
+  const [newItem, setNewItem] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    setTodos((currentTodos) => {
+      return [
+        ...currentTodos,
+        { id: crypto.randomUUID(), title: newItem, completed: false },
+      ];
+    });
+
+    setNewItem("");
+  }
+
+  function toggleTodo(id, completed) {
+    setTodos((currentTodos) => {
+      return currentTodos.map((todo) => {
+        if (todo.id === id) {
+          return { ...todo, completed };
+        }
+
+        return todo;
+      });
+    });
+  }
+
+  function deleteTodo(id) {
+    setTodos((currentTodos) => {
+      return currentTodos.filter((todo) => todo.id !== id);
+    });
+  }
+
+  function displayTime() {
+    let datetime = new Date();
+    document.getElementById("time").textContent = datetime;
+  }
+
+  function updateTime() {
+    let timeDisplayed = document.getElementById("time");
+    let dateString = new Date().toLocaleString();
+    let formattedString = dateString.replace(",", "-");
+    timeDisplayed.textContent = formattedString;
+  }
+
+  displayTime();
+  setInterval(updateTime, 1000);
+
+  return (
+    <>
+      <span id="time"></span>
+      <form onSubmit={handleSubmit} className="new-item-form">
+        <div className="form-row">
+          <label htmlFor="item">New Daily Task</label>
+          <input
+            value={newItem}
+            onChange={(e) => setNewItem(e.target.value)}
+            type="text"
+            id="item-input"
+          />
+        </div>
+        <button className="form-button">Add</button>
+      </form>
+      <h1 className="list-title">Daily Tasks List</h1>
+      <ul className="list">
+        {todos.length === 0 && "No Tasks Created"}
+        {todos.map((todo) => {
+          return (
+            <li key={todo.id}>
+              <label>
+                <input
+                  type="checkbox"
+                  checked={todo.completed}
+                  onChange={(e) => toggleTodo(todo.id, e.target.checked)}
+                />
+                {todo.title}
+              </label>
+              <button
+                onClick={() => deleteTodo(todo.id)}
+                className="delete-button-list"
+              >
+                Delete
+              </button>
+            </li>
+          );
+        })}
+      </ul>
+    </>
+  );
+}
